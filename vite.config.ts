f21c89e60be497c5f1545877e20f02ab39f9f10b
ArtyomInc <artiom.game@gmail.com>
2024-11-13 16:26:57 +0100

    remove raw loader

--- vite.config.ts
+++ vite.config.ts
@@ -5,23 +5,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      plugins: [
-        {
-          name: 'raw-loader',
-          transform(code, id) {
-            if (id.endsWith('.vue')) {
-              return {
-                code: `export default ${JSON.stringify(code)}`,
-                map: null
-              }
-            }
-          }
-        }
-      ]
-    }
-  },
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
